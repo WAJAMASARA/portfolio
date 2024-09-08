81be43b46757f5666bd235b2c67c51ce7c89857e
W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>
2024-09-09 00:21:39 +0530

    create the banner section Signed-off-by: W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>

---
+++ src/components/Banner.js
@@ -0,0 +1,69 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import HeaderImage from "../assets/image/header-img.svg";
+import { useState, useEffect } from "react";
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Web Developer", "Mobile Developer", "Animator"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [delta, text, tick]);
+
+  return (
+    <section className="Banner" id="home">
+      <Container>
+        <Row className="align-item-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portfolio.</span>
+            <h1>
+              {`Hi I'm webdecoded `}
+              <span className="wrap">{text}</span>
+            </h1>
+            <p>Hello I'm software engineer</p>
+            <button onClick={() => console.log("connect")}>
+              Let's connect <ArrowRightCircle size={25} />
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={HeaderImage} alt="Header Image" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
