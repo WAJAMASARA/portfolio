8a53e73805fe7dfc844a041eb841a403cbad5c58
W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>
2024-09-11 16:07:20 +0530

    skills js is created and css added Signed-off-by: W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
-import { NavBar } from './components/NavBar';
-import { Banner } from './components/Banner';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import logo from "./logo.svg";
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <NavBar /> 
+      <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
