8df840e585391316a63231d1b402ceaa15cdf4d4
W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>
2024-09-17 23:51:05 +0530

    Banner is compled Signed-off-by: W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,8 @@ import { Col, Container, Row } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import HeaderImage from "../assets/image/header-img.svg";
 import { useState, useEffect } from "react";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
@@ -49,15 +51,25 @@ export const Banner = () => {
       <Container>
         <Row className="align-item-center">
           <Col xs={12} md={6} xl={7}>
-            <span className="tagline">Welcome to my Portfolio.</span>
-            <h1>
-              {`Hi I'm webdecoded `}
-              <span className="wrap">{text}</span>
-            </h1>
-            <p>Hello I'm software engineer</p>
-            <button onClick={() => console.log("connect")}>
-              Let's connect <ArrowRightCircle size={25} />
-            </button>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <span className="tagline">Welcome to my Portfolio.</span>
+                  <h1>
+                    {`Hi I'm webdecoded `}
+                    <span className="wrap">{text}</span>
+                  </h1>
+                  <p>Hello I'm software engineer</p>
+                  <button onClick={() => console.log("connect")}>
+                    Let's connect <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              )}
+            </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
             <img src={HeaderImage} alt="Header Image" />
