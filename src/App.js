eec1a1754f16cfa2f947b3f5f68b4b49f8f07981
W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>
2024-09-17 23:07:43 +0530

    footer structure created and added to app.js Signed-off-by: W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Footer />
     </div>
   );
 }
